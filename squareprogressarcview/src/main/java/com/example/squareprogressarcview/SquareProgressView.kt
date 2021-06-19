9b7cc195ef01abbd4a3f0b6d45053cabe9c10776
Anwesh43 <anweshthecool0@gmail.com>
2021-06-20 02:31:16 +0530

    adding constants and libraries

--- squareprogressarcview/src/main/java/com/example/squareprogressarcview/SquareProgressView.kt
+++ squareprogressarcview/src/main/java/com/example/squareprogressarcview/SquareProgressView.kt
@@ -1,2 +1,28 @@
 package com.example.squareprogressarcview
 
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.Canvas
+import android.graphics.RectF
+
+val colors : Array<Int> = arrayOf(
+    "#f44336",
+    "#01579B",
+    "#00C853",
+    "#FFC107",
+    "#2962FF"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 4
+val scGap : Float = 0.02f / parts
+val strokeFactor : Float = 90f
+val squareSizeFactor : Float = 9.9f
+val rFactor : Float = 2.9f
+val delay : Long = 20
+val rot : Float = 270f
+
