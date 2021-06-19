5e6ac8b719b3fd6303de8986268ccf7da99e01c4
Anwesh43 <anweshthecool0@gmail.com>
2021-06-20 02:49:43 +0530

    adding SPNode class

--- squareprogressarcview/src/main/java/com/example/squareprogressarcview/SquareProgressView.kt
+++ squareprogressarcview/src/main/java/com/example/squareprogressarcview/SquareProgressView.kt
@@ -122,4 +122,45 @@ class SquareProgressView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class SPNode(var i : Int, val state : State = State()) {
+
+        private var next : SPNode? = null
+        private var prev : SPNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i  < colors.size - 1) {
+                next = SPNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSPANode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SPNode {
+            var curr : SPNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
