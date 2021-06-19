4b5f64e0a01ec9a3dde43c5eecb706f9e00f2f85
Anwesh43 <anweshthecool0@gmail.com>
2021-06-20 03:00:57 +0530

    adding view in activity and fullscreen support

--- app/src/main/java/com/example/linkedsquareprogressarcview/MainActivity.kt
+++ app/src/main/java/com/example/linkedsquareprogressarcview/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.linkedsquareprogressarcview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.example.squareprogressarcview.SquareProgressView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        SquareProgressView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
